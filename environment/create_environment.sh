55e9813f08e78451474ef2b9eeb16263dc50ace5
Chris Diehl <chris@thedataguild.com>
2020-10-07 17:32:50 -0600

    Added nbdime to environment/create_environment.sh

--- environment/create_environment.sh
+++ environment/create_environment.sh
@@ -3,4 +3,4 @@ conda create -y -n cafo anaconda==2020.07
 conda activate cafo
 
 pip install gdal==3.0.2 geopandas==0.8.1 rasterio==1.1.0
-pip install streamlit pydeck plotly
+pip install streamlit pydeck plotly nbdime
